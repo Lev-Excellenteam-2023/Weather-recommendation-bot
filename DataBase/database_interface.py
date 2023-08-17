a34fc3239cbcf08f980c95d74da832055fe00654
David Cohen <deiv464@gmail.com>
2023-08-17 11:55:13 +0300

    refactor db to class

--- DataBase/database_interface.py
+++ DataBase/database_interface.py
@@ -1,56 +1,71 @@
 from datetime import datetime
 from typing import Dict, Any, Optional
-
 import firebase_admin
 from firebase_admin import credentials, db
-
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {'databaseURL': "https://telegrambot-96f0c-default-rtdb.firebaseio.com"})
-
-# Access the Firestore database
-ref = db.reference("/")
+from shared.consts import DATABASE_URL, SERVICE_ACCOUNT_KEY_PATH
 
 
-def get_user(user_id: str) -> Optional[Dict[str, Any]]:
+class FirebaseHandler:
     """
-    Retrieve user data from the database.
-
-    Args:
-        user_id (str): The ID of the user.
-
-    Returns:
-        Optional[Dict[str, Any]]: The user data as a dictionary, or None if user does not exist.
+    A utility class for interacting with Firebase Realtime Database.
     """
-    return ref.child(user_id).get()
 
+    def __init__(self):
+        """
+        Initialize the FirebaseHandler instance.
 
-def user_exist(user_id: str) -> bool:
-    """
-    Check if a user exists in the database.
+        Reads the credentials from the provided service account key file and
+        initializes the Firebase app with the specified database URL.
 
-    Args:
-        user_id (str): The ID of the user.
+        Note:
+            Make sure to configure the 'DATABASE_URL' and 'service_account_key_path'
+            constants in the 'const' module.
 
-    Returns:
-        bool: True if the user exists, False otherwise.
-    """
-    return get_user(user_id) is not None
+        """
+        cred = credentials.Certificate(SERVICE_ACCOUNT_KEY_PATH)
+        firebase_admin.initialize_app(cred, {'databaseURL': DATABASE_URL})
+        self.ref = db.reference("/")
 
+    def get_user(self, user_id: str) -> Optional[Dict[str, Any]]:
+        """
+        Retrieve user data from the database.
 
-def add_user_indication(user_id: str, date: datetime, **kwargs: Dict) -> None:
-    """
-    Add user indication data to the database for a specific date.
+        Args:
+            user_id (str): The ID of the user.
 
-    Args:
-        user_id (str): The ID of the user.
-        date (datetime): The date and time of the indication.
-        **kwargs (Dict): Additional data to be added as key-value pairs.
-    """
-    date_str = date.strftime('%Y-%m-%d %H:%M:%S')  # Convert datetime to string
-    user_ref = ref.child(user_id)
-    user_data = user_ref.get()
+        Returns:
+            Optional[Dict[str, Any]]: The user data as a dictionary, or None if user does not exist.
+        ValueError: If user_id is not a string, or user_id begins with '/'.
+        """
+        return self.ref.child(user_id).get()
 
-    if user_data is None:
-        user_ref.set({date_str: kwargs})
-    else:
-        user_ref.child(date_str).set(kwargs)
+    def user_exist(self, user_id: str) -> bool:
+        """
+        Check if a user exists in the database.
+
+        Args:
+            user_id (str): The ID of the user.
+
+        Returns:
+            bool: True if the user exists, False otherwise.
+        ValueError: If user_id is not a string, or user_id begins with '/'.
+        """
+        return self.get_user(user_id) is not None
+
+    def add_user_indication(self, user_id: str, date: datetime, k_args: Dict) -> None:
+        """
+        Add user indication data to the database for a specific date.
+
+        Args:
+            user_id (str): The ID of the user.
+            date (datetime): The date and time of the indication.
+            k_args (Dict): Additional data to be added as key-value pairs.
+        """
+        date_str = date.strftime('%Y-%m-%d %H:%M:%S')  # Convert datetime to string
+        user_ref = self.ref.child(user_id)
+        user_data = user_ref.get()
+
+        if user_data is None:
+            user_ref.set({date_str: k_args})
+        else:
+            user_ref.child(date_str).set(k_args)
