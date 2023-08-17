4f9f64c7932d0891be5caee396d813b2dc612c50
David Cohen <deiv464@gmail.com>
2023-08-17 13:37:44 +0300

    add weather url

--- shared/consts.py
+++ shared/consts.py
@@ -12,3 +12,6 @@ class FeelingCategories(Enum):
 
 DATABASE_URL = "https://telegrambot-96f0c-default-rtdb.firebaseio.com"
 SERVICE_ACCOUNT_KEY_PATH = path.join('DataBase', "serviceAccountKey.json")
+
+WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
+WEATHER_ENDPOINT = f"http://api.weatherapi.com/v1/history.json"
