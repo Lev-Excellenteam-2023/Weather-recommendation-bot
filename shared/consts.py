d779be4034a52dcd44bed6fd178a6705226a786c
Shai Axelrod <saxelrod@g.jct.ac.il>
2023-08-27 15:39:47 +0300

    add newline in EOF

--- shared/consts.py
+++ shared/consts.py
@@ -15,3 +15,4 @@ SERVICE_ACCOUNT_KEY_PATH = path.join('DataBase', "serviceAccountKey.json")
 
 WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
 WEATHER_ENDPOINT = f"http://api.weatherapi.com/v1/history.json"
+
